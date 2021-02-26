3ecb836025f8cde6491d156ac0ceb18339382c7b
ahernot <70817470+ahernot@users.noreply.github.com>
2021-02-26 15:26:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import coordinator as C
 
+
+if __name__ == '__main__':
+    C.add_watermark(
+        image_path='./resources/images/IMG_5613.JPG',
+        watermark_path='./resources/watermarks/copyright-whitetransp.tiff',
+        watermark_size=(None, 0.1),
+        watermark_position=(50, 1.0),
+        image_size=1500,
+        opacity=0.35
+    )
