637c2b9c47dd64e39eed083eb1fa2bdf813a0375
ahernot <70817470+ahernot@users.noreply.github.com>
2021-02-26 15:29:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import coordinator as C
+import coordinator as Coordinator
 
+
+# Add batch processing
 
 if __name__ == '__main__':
-    C.add_watermark(
+    Coordinator.add_watermark(
         image_path='./resources/images/IMG_5613.JPG',
         watermark_path='./resources/watermarks/copyright-whitetransp.tiff',
+        output_path='./output/output.png',
         watermark_size=(None, 0.1),
         watermark_position=(50, 1.0),
         image_size=1500,
