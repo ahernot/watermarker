68a0cc8030a345b2508c6cde25bf290b54d628e2
ahernot <70817470+ahernot@users.noreply.github.com>
2021-02-26 15:29:29 +0100

    Update coordinator.py

--- coordinator.py
+++ coordinator.py
@@ -1,7 +1,7 @@
 # Layer merging solution from https://stackoverflow.com/questions/40895785/using-opencv-to-overlay-transparent-image-onto-another-image
 import cv2
 
-import images as Images
+import auxiliary_functions as AuxFunc
 
 
 
@@ -12,6 +12,7 @@ import images as Images
 def add_watermark(
         image_path: str,
         watermark_path: str,
+        output_path: str,
         watermark_size: tuple or int or float = (None, None),
         watermark_position: tuple = (1.0, 1.0),
         image_size: tuple or int or float = (1.0, 1.0),
@@ -20,6 +21,7 @@ def add_watermark(
     Add watermark to image. Remove EXIF tags.
     :param image_path:
     :param watermark_path:
+    :param output_path: Output save path
     :param watermark_size: Tuple of percentages/pixels: (width-percentage, height-percentage), leave one as None for proportions // , or pixel value (for largest axis)
     :param watermark_position: Tuple of percentages/pixels: (width-percentage, height-percentage) from top left
     :param image_size: Tuple of percentages/pixels, or pixel value (for largest axis)
@@ -33,19 +35,19 @@ def add_watermark(
         mask_opacity = kwargs['opacity']
 
     # Load image and watermark
-    image, i_height, i_width, i_depth = Images.imread_rgba(image_path)
-    watermark, w_height, w_width, w_depth = Images.imread_rgba(watermark_path)
+    image, i_height, i_width, i_depth = AuxFunc.imread_rgba(image_path)
+    watermark, w_height, w_width, w_depth = AuxFunc.imread_rgba(watermark_path)
 
     # Calculate new sizes
-    i_width, i_height = Images.set_image_size(i_width, i_height, image_size)
-    w_width, w_height = Images.set_watermark_size(i_width, i_height, w_width, w_height, watermark_size)
+    i_width, i_height = AuxFunc.set_image_size(i_width, i_height, image_size)
+    w_width, w_height = AuxFunc.set_watermark_size(i_width, i_height, w_width, w_height, watermark_size)
 
     # Resize
     image_resized = cv2.resize(image, (i_width, i_height), interpolation=cv2.INTER_AREA)
     watermark_resized = cv2.resize(watermark, (w_width, w_height), interpolation=cv2.INTER_AREA)
 
     # Pad watermark
-    watermark_layer = Images.pad(watermark_resized, i_width, i_height, w_width, w_height, watermark_position)
+    watermark_layer = AuxFunc.pad(watermark_resized, i_width, i_height, w_width, w_height, watermark_position)
 
     # Merge layers
     w_img = watermark_layer[:, :, :3]
@@ -57,4 +59,4 @@ def add_watermark(
     image_resized = ((1.0 - w_mask) * image_resized)[:, :, :3] + w_mask * w_img
 
     # Save image
-    cv2.imwrite('out.png', image_resized)
+    cv2.imwrite(output_path, image_resized)
