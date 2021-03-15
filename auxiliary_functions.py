f2e8dfba68383a35efacec58c59c9b26cc4224aa
ahernot <70817470+ahernot@users.noreply.github.com>
2021-03-15 09:57:42 +0100

    add calc_max_size, general set_size

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -23,17 +23,61 @@ def imread_rgba(path):
     return img, height, width, depth
 
 
-def set_watermark_size(i_width, i_height, w_width, w_height, watermark_size):
+def calc_max_size(i1_width, i1_height, i2_width, i2_height):
+    """
+    Calculate maximum size for image 2 inside of image 1 with no overflow and no stretching.
+    :param i1_width:
+    :param i1_height:
+    :param i2_width:
+    :param i2_height:
+    :return:
+    """
+
+    # Calculate ratios
+    i1_ratio = i1_width / i1_height
+    i2_ratio = i2_width / i2_height
+
+    # Calculate maximum watermark size
+    if i2_ratio >= i1_ratio:
+        max_width = i1_width
+        max_height = i2_width / i2_ratio
+    else:
+        max_height = i1_height
+        max_width = i2_height * i2_ratio
+
+    return i2_width, i2_height
+
+
+
+
+# TODO
+# abs(width) for percentage checks
+# and width-val for negative values; -1 should remove 1 pixel only
+def set_watermark_size(i_shape, w_shape, w_resize):
+    """
+    Set watemrark size
+
+    reference_size = i_shape, for both watermark and image (used to resize, max_size)
+
+    :param i_shape:
+    :param w_shape:
+    :param w_resize:
+    :return:
+    """
+
+    # Unpack shapes
+    i_width, i_height = i_shape
+    w_width, w_height = w_shape
 
     # Set the largest dimension
-    if type(watermark_size) in (int, float):
+    if type(w_resize) in (int, float):
         if w_width >= w_height:
-            watermark_size = (watermark_size, None)
+            watermark_size = (w_resize, None)
         else:
-            watermark_size = (None, watermark_size)
+            watermark_size = (None, w_resize)
 
     # Preserve dimensions
-    if watermark_size in (None, (None, None)):
+    if w_resize in (None, (None, None)):
         watermark_size = (1.0, 1.0)
 
     # Calculate aspect ratios
@@ -41,15 +85,16 @@ def set_watermark_size(i_width, i_height, w_width, w_height, watermark_size):
     w_ratio = w_width / w_height
 
     # Unpack size tuple
-    width, height = watermark_size
+    width, height = w_resize
 
-    # Convert all percentages to pixel counts
+    # Convert all percentages to pixel counts (if percentages given), using reference_size
     if (width is not None) and (width <= 1):
         width = width * i_width  # read as percentage of image width
     if (height is not None) and (height <= 1):
         height = height * i_height  # read as percentage of image height
 
-    # Calculate maximum watermark size
+    # Calculate maximum watermark size, using reference_size
+    # TODO: remove from here
     if w_ratio >= i_ratio:
         max_width = i_width
         max_height = w_width / w_ratio
@@ -70,6 +115,9 @@ def set_watermark_size(i_width, i_height, w_width, w_height, watermark_size):
         height = width / w_ratio
 
     return int(width), int(height)
+
+
+
 
 
 def set_image_size(i_width, i_height, image_size):
@@ -97,6 +145,13 @@ def set_image_size(i_width, i_height, image_size):
     if (height is not None) and (height <= 1):
         height = height * i_height  # read as percentage
 
+    """# Apply max dimensions
+    max_width, max_height = max_size
+    if width is not None:
+        width = min(width, max_width)
+    if height is not None:
+        height = min(height, max_height)"""
+
     # Remove None to preserve aspect ratio
     if width is None:
         width = i_ratio * height
@@ -104,6 +159,67 @@ def set_image_size(i_width, i_height, image_size):
         height = width / i_ratio
 
     return int(width), int(height)
+
+
+
+
+def set_size(width, height, target_size, reference_size):
+    """
+    Set image/watermark size based on bounding container and resizing requirement.
+    :param width: Original width
+    :param height: Original height
+    :param target_size: Target size
+    :param reference_size: Size of the bounding box container (affects percentage sizes and max size)
+    :return: New size
+    """
+
+    # Calculate aspect ratio
+    aspect_ratio = width / height
+
+    # Unpack reference size
+    r_width, r_height = reference_size
+
+    # Unpack target_size
+    if target_size in (None, (None, None)):  # preserve dimensions
+        t_width, t_height = 1.0, 1.0
+
+    elif type(target_size) in (int, float):  # set the largest dimension (if only one size value given)
+        if aspect_ratio >= 1:
+            t_width, t_height = target_size, None
+        else:
+            t_width, t_height = None, target_size
+    else:  # regular unpacking
+        t_width, t_height = target_size
+
+    # Read target_size as percentages (if values <= 1)
+    if (t_width is not None) and (t_width <= 1):
+        width *= r_width
+    if (t_height is not None) and (t_height <= 1):
+        height *= r_height
+
+    # Calculate max dimensions
+    max_width, max_height = width, height  # TODO: replace with function call
+
+    # Apply max dimensions
+    if width is not None:
+        width = min(width, max_width)
+    if height is not None:
+        height = min(height, max_height)
+
+    # Fill None with aspect ration constraint
+    if width is None:
+        width = aspect_ratio * height
+    elif height is None:
+        height = width / aspect_ratio
+
+    return int(width), int(height)
+
+
+
+
+
+
+
 
 
 
