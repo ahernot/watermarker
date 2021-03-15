708a25d5cce6acd5d13998fde93964873773874a
ahernot <70817470+ahernot@users.noreply.github.com>
2021-03-15 09:58:00 +0100

    batch processing test

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ import coordinator_wrapper as CoordinatorW
 
 if __name__ == '__main__':
 
+    """
     CoordinatorW.add_watermark_batch(
         watermark_path='./resources/watermarks/copyright-whitetransp.tiff',
         watermark_size=(None, 0.1),
         watermark_position=(50, 1.0),
         image_size=1500,
+        opacity=0.35
+    )
+    """
+
+    CoordinatorW.add_watermark_batch(
+        watermark_path='./resources/watermarks/copyright-whitetransp.tiff',
+        watermark_size=(None, 0.13),
+        watermark_position=(50, 1.0),
+        image_size=1000,
         opacity=0.35
     )
