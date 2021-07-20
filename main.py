02324023837c197a4df6677e20b5760df641d0eb
ahernot <70817470+ahernot@users.noreply.github.com>
2021-07-20 13:17:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,19 @@ import batch
 
 if __name__ == '__main__':
 
-    # Change path to ~/backend/processing/
+    # Change path to the parent directory
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
 
     batch.run(path=preferences.SOURCE_PATH)
+
+
+# todo: make it a module, with an __init__
+"""
+usage:
+watermarker.Batch (
+    watermarks = () # optional
+)
+
+"""
